9d875367f63a9bac1f182357837d4572dec5c8c1
IofSoft <oliveira.ian3@gmail.com>
2024-06-07 23:48:32 -0300

    initial

--- Binarytree.c
+++ Binarytree.c
@@ -1,43 +1,39 @@
 #include "BinaryTree.h"
 
 void menu(t_binary_tree *tree){
-    char *input = "";
     char *operation = "";
-    char *tree_code;
-    while(!strcmp(operation, "exit")){
+    while(strcmp(operation, "exit") != 1){
+        char *input = "";
         fgets(input, sizeof(input), stdin);
         operation = get_operation(input);
-        switch(operation){
-            case "create":
 
-                break;
+        if(strcmp(operation,"create")){
+            char *tree_code = get_tree_code(input);
+            tree->root = build_tree(tree_code);
+            continue;
+        }
 
-            case "pre":
-                pre_order(tree->root);
-                break;
+        if(strcmp(operation,"pre")){
+            pre_order(tree->root);
+            continue;
+        }
 
-            case "in":
-                in_order(tree->root);
-                break;
+        if(strcmp(operation,"in")){
+            in_order(tree->root);
+            continue;
+        }
+        if(strcmp(operation, "post")){
+            post_order(tree->root);
+            continue;
+        }
+        if(strcmp(operation, "height")){
+            continue;
+        }
 
-            case "post":
-                post_order(tree->root);
-                break;
-
-            case "height":
-
-                break;
-            case "print":
-
-                break;
+        if(strcmp(operation, "print")){
+            print_tree(tree->root, 0);
         }
     }
-}
-char* get_tree_code(char* input){
-    char *index = strpbrk(input,"(");
-    (*index)--;
-    char *tree_code = strdup(index);
-    return tree_code;
 }
 
 char* get_operation(char* input){
@@ -47,6 +43,73 @@ char* get_operation(char* input){
     return operation;
 }
 
+char* get_tree_code(char* input){
+    char *index = strpbrk(input,"(");
+    (*index)--;
+    char *tree_code = strdup(index);
+    return tree_code;
+}
+
+t_node* build_tree(char *tree_code){
+    t_stack *stack = create(50);
+    t_node *root = NULL;
+    t_node *present = NULL;
+
+    while(*tree_code){
+
+        if(*tree_code == '('){
+            tree_code++;
+            continue;
+        }
+
+        if(*tree_code == ')'){
+            present = pop(stack);
+            tree_code++;
+            continue;
+        }
+
+        if(*tree_code == ','){
+            tree_code++;
+            continue;
+        }
+
+        t_node *node = create_node(*tree_code);
+
+        if(root == NULL) root = node;
+        else{
+            if(present->left == NULL) present->left = node;
+            else present->right = node;
+        }
+        push(stack, node);
+        present = node;
+        tree_code++;
+    }
+    return root;
+}
+
+t_binary_tree* create_tree(){
+    t_binary_tree *tree =  malloc (sizeof (t_binary_tree));
+    tree->root = NULL;
+    return tree;
+}
+
+t_node* create_node(char item){
+    t_node *node =  malloc (sizeof (t_node));
+    node->item = item;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+void print_tree(t_node *root, int height){
+    if(root == NULL) return;
+    print_tree(root->right, height + 1);
+    for(int i = 0; i < height; i++){
+        printf("    ");
+    }
+    printf("%c\n", root->item);
+    print_tree(root->left, height + 1);
+}
 
 void pre_order(t_node *node) {
     if (node != NULL) {
